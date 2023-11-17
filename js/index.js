5dbba37087ea630c83c862af9dbd6713dac170e9
Nick Heerschap <njheerschap@gmail.com>
2023-11-16 22:05:57 -0500

    Enable used to adjust the size of the grid by inputting a width.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('.container'); 
+const button = document.querySelector('button');
+
 function createGrid(size= 16) {
     for (let i = 0; i < size; i++){
         const gridItem = document.createElement('div');
@@ -7,23 +9,36 @@ function createGrid(size= 16) {
     }
 }
 
+function changeGrid() {
+    container.innerHTML = ""
+    const widthField = document.querySelector('#width-field');
+    const width = Number(widthField.value);
+    const widthSquared = width * width 
+    container.style.gridTemplateColumns = `repeat(${width}, 1fr)`
+    createGrid(widthSquared)
+}
+
 function changeOnHover() {
 
     function addColor(e) {
         e.target.classList.add('colored');
     }
 
-
+    function addColorOnMove() {
+        container.addEventListener('mouseover', addColor)
+    }
 
     container.addEventListener('mousedown', addColor)
-    
-//     container.addEventListener('mouseout', (e) => {
-//         if (e.target.classList.contains('colored')) {
-//             e.target.classList.remove('colored')
-//         }
-//     })
+    container.addEventListener('mousedown', addColorOnMove)
+    container.addEventListener('mouseup', () => {
+        container.removeEventListener('mouseover', addColor)
+    })
 }
 
 
 createGrid()
 changeOnHover()
+
+button.addEventListener('click', () => {
+    changeGrid()
+})
